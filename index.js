8bb4f1a6ddb1c75f7ac77208af1a7b0fae5c872e
Your Name <your.email@example.com>
2025-02-04 11:47:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { resolve } = require("path");
+
+const app = express();
+const port = 3010;
+
+app.use(express.static("static"));
+require("dotenv").config();
+
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === "true",
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"));
+});
+app.get("/config", (req, res) => {
+  res.json(config);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
